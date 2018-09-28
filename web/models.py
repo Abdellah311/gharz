addfc4caab1880d74c583ae5f475344779ea4808
abi <aromame_311@yahoo.com>
2018-09-28 04:00:37 -0400

    kharj model unja ast

--- web/models.py
+++ web/models.py
@@ -2,5 +2,20 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+class kharj(models.Model):
+    matn= models.CharField(max_length=300)
+    tarikh= models.DateTimeField()
+    meghdar= models.BigIntegerField()
+    karbar= models.ForeignKey(User)
+    """docstring for kharj models.Model__
+    text= models.charField(max_length=300)
+    date= models.DateTimefield()
+    (self, arg):
+        super(kharj_models.Model()
+        text= models.charField(max_length=300)
+        date= models.DateTimefield()
 
-# Create your models here.
+        self.arg = arg
+
+# Create your models here."""
